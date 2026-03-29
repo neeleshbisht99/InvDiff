ad0d8c89f1326082e5b57059c87b42f611d5feb0
Neelesh Bisht <neeleshbisht99@gmail.com>
2026-03-29 12:36:03 +0000

    update metashift metrics script

--- invdiff/scripts/metashift_metrics.py
+++ invdiff/scripts/metashift_metrics.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-root = "/shared/scratch/0/home/v_neelesh_bisht/projects/InvDiff/sweeps/output/CCDiff-MetaShift/"
+root = "/shared/scratch/0/home/v_neelesh_bisht/projects/InvDiff/sweeps/output/VisDiff-Metashift/"
 
 data = pd.read_csv(root + 'runs.csv')
 
@@ -8,10 +8,9 @@ f = open(root+"output.txt", 'w')
 
 print("Metashift Metrics", file=f)
 print("#### CCDiff (Image & Text(CLIP) Evaluator(GPT-4))", file=f)
-print("#Group A / Class 0", file=f)
 arr0 = [
     ["Dataset ", "acc@1 ", "acc@5 "],
-    ["MimicCxrImageSets    ", f"{round(data['Group A/acc@1'].mean(), 2)}   ", f"{round(data['Group A/acc@5'].mean(), 2)}  "]
+    ["Metashift    ", f"{round(data['acc@1'].mean(), 2)}   ", f"{round(data['acc@5'].mean(), 2)}  "]
 ]
 
 for row in arr0:
